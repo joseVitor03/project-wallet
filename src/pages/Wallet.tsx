883e7f7b5fcdaed4dfe5af9d31cb1c2d3483c8dd
joseVitor03 <jv681033@gmail.com>
2023-08-30 17:38:45 -0300

    req 2

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -5,7 +5,9 @@ function Wallet() {
   const { email } = useSelector((state: GlobalState) => state.user);
   return (
     <header>
-      <h4>{email}</h4>
+      <h4 data-testid="header-currency-field">BRL</h4>
+      <h4 data-testid="total-field">Despesa Total: 0</h4>
+      <h6 data-testid="email-field">{email}</h6>
     </header>
   );
 }
